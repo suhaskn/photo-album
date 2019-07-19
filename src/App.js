26ba3635ee4ea2aa436a3567c0f940b6050b3892
Suhas <suhaskn09@gmail.com>
2019-07-19 11:51:30 +0200

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Provider>
   );
 }
 
